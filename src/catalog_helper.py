0f2e2e8580c8e0d109e62d0daabd55457c3c1df7
russell925 <emiliorussell.manalo@ontariotechu.net>
2025-01-24 16:45:22 -0500

    cli

---
+++ src/catalog_helper.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import csv
+
+def edit_csv_entry():
+    df = pd.read_csv('data/catalog_database.csv')
+    
+    id = input("What is the ID of the entry to edit: ")
+    print(id)
+            
+            
+edit_csv_entry()
+
+
+#called when need to print whole database
+def printcsv():
+  with open('data\catalog_database.csv', mode ='r') as csvfile:
+    database = csv.reader(csvfile)
+    for lines in database:
+      print(lines)
+
+def add():
+        x =  input("id to add:")
+        y = input ("name to add:")
+        z = input ("description to add:")
+        data = {'ID':[x], "Name" :[y], "Description":[z]}
+        data = pd.DataFrame(data)
+        data.to_csv('data/catalog_database.csv', mode='a', index=False, header=False)
