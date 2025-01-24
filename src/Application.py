7e833efe0d0eac7590d3109055d34a32d64d5674
IsabellaSmithOTU <isabella.smith1@ontariotechu.net>
2025-01-24 16:51:14 -0500

    Update Application.py

--- src/Application.py
+++ src/Application.py
@@ -5,8 +5,17 @@ def main ():
     # parser = argparse.ArgumentParser()
     # parser.add_argument("filename", nargs='1', metavar='file', type='str', help="File to be read")
     # args = parser.parse_args()
-
+    
     catalog_helper.printcsv()
     catalog_helper.edit_csv_entry()
+    input = 0
+    while(true):
+        if(input == 6):
+            break
+        else:
+            print("1. View ID description")
+            print("2. Add to file")
+            print("3. Edit file")
+            
 
     
