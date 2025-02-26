7b720f37e714ee6746654292fbd17626f5767364
JMatthewK <matthewkassapian4@gmail.com>
2025-02-26 13:03:18 -0500

    Basic catalog display of items in terminal works

--- src/CatalogApp.java
+++ src/CatalogApp.java
@@ -3,14 +3,19 @@ import java.util.*;
 
 public class CatalogApp {
     public static void main(String[] args) throws IOException {
+        // Create a csvparser object
         CsvParser csvParser = new CsvParser();
+        // Make a list of clothingItems to use
         List<ClothingItem> clothingItemList;
 
+        // Use the csvparser object with the catalogCSV in our data folder
+        // Try an catch incase it doesn't work
         try{
             clothingItemList = csvParser.parseCsv("data/CatalogData.csv");
         } catch (java.lang.Exception e) {
             throw new IOException("error parsing file", e);
         }
+        // Print out all of the data in this catalog
         System.out.println("Catalog Data");
         for (ClothingItem item : clothingItemList){
             System.out.println();
