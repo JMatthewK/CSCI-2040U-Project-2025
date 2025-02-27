ef08b7c363e76c1d62ff1fb0097de0d2b34a3d99
JMatthewK <matthewkassapian4@gmail.com>
2025-02-27 11:23:17 -0500

    Checks CSV for appropriate attribute length

--- src/main/java/CsvParser.java
+++ src/main/java/CsvParser.java
@@ -14,19 +14,20 @@ public class CsvParser {
         while((line = br.readLine()) != null){
             // Comma delimiter to read the CSV file attribute by attribute
             String[] attributes = line.split(",");
+            if(attributes.length == 10){
+                int id = Integer.parseInt(attributes[0]);
+                String name = attributes[1];
+                String brand = attributes[2];
+                String color = attributes[3];
+                String category = attributes[4];
+                double price = Double.parseDouble(attributes[5]);
+                String material = attributes[6];
+                String style = attributes[7];
+                String fit = attributes[8];
 
-            int id = Integer.parseInt(attributes[0]);
-            String name = attributes[1];
-            String brand = attributes[2];
-            String color = attributes[3];
-            String category = attributes[4];
-            double price = Double.parseDouble(attributes[5]);
-            String material = attributes[6];
-            String style = attributes[7];
-            String fit = attributes[8];
-
-            ClothingItem item = new ClothingItem(id, name, brand, color, category, price, material, style, fit);
-            clothingItems.add(item);
+                ClothingItem item = new ClothingItem(id, name, brand, color, category, price, material, style, fit);
+                clothingItems.add(item);
+            }
         }
         // Close bufferedReader
         br.close();
